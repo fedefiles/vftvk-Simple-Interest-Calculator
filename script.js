3c3162162ad76e06422f8dc7916abd4982fd8bcb
fedefiles <intimarca@gmail.com>
2021-12-18 01:25:43 -0300

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	//declaring variables
+
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	//calculations
+
+	var interest = principal * years * rate /100;
+	var year = new Date().getFullYear()+parseInt(years);
+
+	//validate the principal. If negative or 0 alert user and focus on the input  
+	
+    	if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+	}
+	
+	//show result
+
+	document.getElementById("result").innerHTML="If you deposit<mark> 	"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%\</mark><br\>You will 	receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+
 }
-        
+
+	
+	//function reads the value of the range slider and displays it...
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
